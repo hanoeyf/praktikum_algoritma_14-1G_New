31ca8a718a42994d7191321a2ecacdfc7169fd50
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-02-20 08:30:52 +0700

    Jobsheet2_percobaan1_14

---
+++ src/percobaan2_14/Buku14.java
@@ -0,0 +1,25 @@
+package percobaan2_14;
+public class Buku14{
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi(){
+        System.out.println("judul: "+judul);
+        System.out.println("pengarang: "+pengarang);
+        System.out.println("halaman: "+halaman);
+        System.out.println("sisa stok: "+stok);
+        System.out.println("harga: "+harga);
+    }
+    void terjual(int jumlah){
+        if (stok>0){
+        stok-=jumlah;}
+        else {System.out.println("stok habis");}
+
+    }
+    void restock(int jumlah){
+        stok += jumlah;
+    }
+    void gantiHarga(int hrg){
+        stok=hrg;
+    }
+}
