7e07b1613a08b22317eb0d2eef9ab25edba9df02
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-03-25 10:20:39 +0700

    jobsheet6-BF&DC-14

---
+++ src/Minggu5/mainFaktorial.java
@@ -0,0 +1,27 @@
+package Minggu5;
+
+import java.util.Scanner;
+
+public class mainFaktorial {
+    public static void main(String[] args) {
+        Scanner sc14 = new Scanner(System.in);
+        System.out.println("-------------------");
+        System.out.println("masukkan jumlah elemen: ");
+        int ijml = sc14.nextInt();
+        faktorial[] fk= new faktorial[10];
+        for (int i = 0; i < ijml; i++) {
+            fk[i] = new faktorial();
+            System.out.print("Masukkan nilai data ke-" + (i+1)+": ");
+            int inilai= sc14.nextInt();
+            fk[i].nilai = inilai;
+        }
+        System.out.println("HASIL - BRUTE FORCE");
+        for (int i = 0; i < ijml; i++) {
+            System.out.println("hasil perhitungan faktorial menggunakan Brute Force adalah: "+ fk[i].faktorialBF(fk[i].nilai));
+        }
+        System.out.println("HASIL - DEVIDDE AND CONQUER:");
+        for (int i = 0; i < ijml; i++) {
+            System.out.println("hasil perhitungan faktorial menggunakan Devide and Conquer adalah: "+ fk[i].faktorialDC(fk[i].nilai));
+        }
+    }
+}
