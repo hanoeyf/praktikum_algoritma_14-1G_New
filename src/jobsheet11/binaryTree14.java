da1163a5277cb1b880b854447bcb50ebdc6c89e8
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-05-30 21:06:26 +0700

    jobshee 11 percobaan 1

---
+++ src/jobsheet11/binaryTree14.java
@@ -0,0 +1,166 @@
+package jobsheet11;
+
+public class binaryTree14 {
+    node14 root;
+
+    public binaryTree14() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    void add(int data) {
+        node14 newNode = new node14(data);
+        if (isEmpty()) {
+            root = newNode;
+        } else {
+            node14 current = root;
+            node14 parent = null;
+            while (true) {
+                parent = current;
+                if (data < current.data) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else if (data > current.data) {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                } else {
+                    // Data sudah ada di tree
+                    return;
+                }
+            }
+        }
+    }
+
+    boolean find(int data) {
+        node14 current = root;
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            } else if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    void traversePreOrder(node14 node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(node14 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    void traverseInOrder(node14 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(node.data + " ");
+            traverseInOrder(node.right);
+        }
+    }
+
+    node14 getSuccessor(node14 del) {
+        node14 successorParent = del;
+        node14 successor = del.right;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree is empty");
+            return;
+        }
+
+        node14 parent = root;
+        node14 current = root;
+        boolean isLeftChild = true;
+
+        while (current.data != data) {
+            parent = current;
+            if (data < current.data) {
+                isLeftChild = true;
+                current = current.left;
+            } else {
+                isLeftChild = false;
+                current = current.right;
+            }
+
+            if (current == null) {
+                System.out.println("Couldn't find data!");
+                return;
+            }
+        }
+
+        // If node to be deleted is a leaf (no children)
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+        // If node to be deleted has only one child (right)
+        else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else if (isLeftChild) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        }
+        // If node to be deleted has only one child (left)
+        else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else if (isLeftChild) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        }
+        // If node to be deleted has two children
+        else {
+            node14 successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+    }
+
+    
+    }
---
+++ src/jobsheet11/node14.java
@@ -0,0 +1,14 @@
+package jobsheet11;
+public class node14 {
+    int data;
+    node14 left;
+    node14 right;
+
+    public node14(){
+    }
+    public node14(int data){
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
